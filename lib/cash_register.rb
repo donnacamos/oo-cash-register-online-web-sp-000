4638369f012f7efb8dce434ccd37efdefcf1002e
Donna Amos <donnacamos.developer@gmail.com>
2019-03-15 02:52:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,6 +25,6 @@ class CashRegister
     if @discount > 0 
       @to_take_off = (price * discount)/100 
       @total -= @to_take_off 
-      
+      return "After the discount the total comes to $#{total}" 
       
 end 
