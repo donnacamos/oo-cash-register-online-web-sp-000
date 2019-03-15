7bac52c2a5e5366a502811f4d922f1c911f235a9
Donna Amos <donnacamos.developer@gmail.com>
2019-03-15 02:51:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,6 +23,7 @@ class CashRegister
   
   def apply_discount 
     if @discount > 0 
-      @to_take_off = (price * discount)/ 100 
+      @to_take_off = (price * discount)/100 
+      
       
 end 
